6271e14ed33ac3a930db5dd961a131ca61782984
lskam <yangkamboy@outlook.com>
2025-12-03 09:38:25 +0800

    complete the downloade scripts

--- MMDocIR/scripts/download_dataset.sh
+++ MMDocIR/scripts/download_dataset.sh
@@ -1,6 +1,6 @@
 if [ ! -d "MMDocIR/Dataset" ]; then
-    mkdir MMDocIR/Dataset
+    mkdir MMDocIR/dataset
 fi
-git clone https://huggingface.co/datasets/MMDocIR/MMDocIR_Evaluation_Dataset.git && mv MMDocIR_Evaluation_Dataset/*.parquet MMDocIR/Dataset/
+git clone https://huggingface.co/datasets/MMDocIR/MMDocIR_Evaluation_Dataset.git && mv MMDocIR_Evaluation_Dataset/*.parquet MMDocIR/dataset/
 mv MMDocIR_Evaluation_Dataset/*.jsonl MMDocIR/dataset/
 rm -rf MMDocIR_Evaluation_Dataset
--- MMDocIR/scripts/download_model.sh
+++ MMDocIR/scripts/download_model.sh
@@ -1,4 +1,4 @@
 if [ ! -d "MMDocIR/checkpoint" ]; then
     mkdir MMDocIR/checkpoint
 fi
-git clone https://hf-mirror.com/BAAI/bge-large-en-v1.5.git MMDocIR/checkpoint/bge-large-en-v1.5
+git clone https://huggingface.co/BAAI/bge-large-en-v1.5.git MMDocIR/checkpoint/bge-large-en-v1.5
