367178e825161ff3870d32a7df598223f9b7419a
lskam <yangkamboy@outlook.com>
2025-12-03 09:52:56 +0800

    complete the downloade scripts

--- MMDocIR/scripts/download_dataset.sh
+++ MMDocIR/scripts/download_dataset.sh
@@ -1,6 +1,7 @@
-if [ ! -d "MMDocIR/Dataset" ]; then
+if [ ! -d "MMDocIR/dataset" ]; then
     mkdir MMDocIR/dataset
 fi
+git lfs install
 git clone https://huggingface.co/datasets/MMDocIR/MMDocIR_Evaluation_Dataset.git && mv MMDocIR_Evaluation_Dataset/*.parquet MMDocIR/dataset/
 mv MMDocIR_Evaluation_Dataset/*.jsonl MMDocIR/dataset/
 rm -rf MMDocIR_Evaluation_Dataset
